cc861abdbf4ffbcc4ec00bb74146552e046d2192
amxmsolis <amx_msolis@hotmail.com>
2024-02-29 23:27:37 -0600

    WK01 Prep3 and prep4

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,48 @@
 using System;
+// WK01 Monica Solis Loops Magic number
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        
+
+        string messageReceived="Nothing";
+        int tryTimes=0;
+        
+        Random magicNumber = new Random();
+        int magicNumberInt= magicNumber.Next(1,100);
+        //Console.WriteLine($"{magicNumberInt}");
+
+        do {
+
+
+            Console.WriteLine("What is your guess?");
+            string guessNumber=Console.ReadLine();
+            int guessNumberInt= int.Parse(guessNumber);
+
+            if (guessNumberInt < magicNumberInt){
+                messageReceived="Higher";
+         
+            }else  if (guessNumberInt > magicNumberInt){
+                messageReceived="Lower";
+              
+            } else{
+                Console.WriteLine("You guessed it!\nDo you want to play? yes or no");
+                string wanaPlay= Console.ReadLine();
+                if (wanaPlay == "yes"){
+                    messageReceived="";
+                }else{
+                    messageReceived="You guessed it!";
+                }
+            }
+            tryTimes++;
+            Console.WriteLine($"{messageReceived}");
+        } while (messageReceived != "You guessed it!");
+
+        Console.WriteLine($"{tryTimes} attemps");
+        
     }
+ 
 }
